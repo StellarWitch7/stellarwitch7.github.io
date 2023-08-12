86c855bdf0c39ede1db669f919a6e3ab8409f8bf
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2023-08-12 01:05:15 -0400

    I am going to stab TypeScript

--- docs/build/SearchBar.js
+++ docs/build/SearchBar.js
@@ -1,10 +1,6 @@
 "use strict";
 let input;
 function updateInput(elem) {
-    console.log('Searching?');
     input = elem.value;
-    if (!input.endsWith('%')) {
-        console.log('Searching!');
-        input = '';
-    }
+    console.log('Searching: "' + { input } + '"');
 }
--- docs/scripts/SearchBar.ts
+++ docs/scripts/SearchBar.ts
@@ -1,11 +1,6 @@
 let input: string;
 
 function updateInput(elem: HTMLInputElement) {
-    console.log('Searching?');
     input = elem.value;
-
-    if (!input.endsWith('%')) {
-        console.log('Searching!');
-        input = '';
-    }
+    console.log('Searching: "' + {input} + '"');
 }
