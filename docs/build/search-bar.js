fed3ebf9f6cf63d745b3ea63d91c0dcc70adb0ab
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2023-08-12 14:49:17 -0400

    Might have figured it out

--- docs/build/search-bar.js
+++ docs/build/search-bar.js
@@ -1,29 +1,34 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const worker_threads_1 = require("worker_threads");
-const tick = new worker_threads_1.Worker('./worker.js', {
-    workerData: {
-        path: './workers/tick.ts'
-    }
-});
-let input = '';
+// import { Worker } from 'worker_threads';
+// const tick = new Worker('./worker.js', {
+//   workerData: {
+//     path: './workers/tick.ts'
+//   }
+// });
+let searchbar = document.getElementById('searchbar');
+let input;
 let searchDelay = 1000;
 let ticksSinceLastChange = 0;
-function updateInput(elem) {
-    input = elem.value;
+function inputChanged() {
+    input = searchbar.value;
     ticksSinceLastChange = 0;
 }
-function search() {
-    console.log(`Searching: "${input}"`);
-}
-tick.on('message', (result) => {
-    if (result == 'tick') {
-        if (ticksSinceLastChange != -1) {
-            ticksSinceLastChange = ticksSinceLastChange + 1;
-        }
-        if (ticksSinceLastChange >= searchDelay) {
-            ticksSinceLastChange = -1;
-            search();
-        }
-    }
-});
+searchbar.addEventListener('oninput', inputChanged);
+// function updateInput(elem: HTMLInputElement) {
+//   input = elem.value;
+//   ticksSinceLastChange = 0;
+// }
+// function search() {
+//   console.log(`Searching: "${ input }"`);
+// }
+// tick.on('message', (result) => {
+//   if (result == 'tick') {
+//     if (ticksSinceLastChange != -1) {
+//       ticksSinceLastChange = ticksSinceLastChange + 1;
+//     }
+//     if (ticksSinceLastChange >= searchDelay) {
+//       ticksSinceLastChange = -1;
+//       search();
+//     }
+//   }
+// });
--- docs/scripts/search-bar.ts
+++ docs/scripts/search-bar.ts
@@ -1,33 +1,41 @@
-import { Worker } from 'worker_threads';
+// import { Worker } from 'worker_threads';
 
-const tick = new Worker('./worker.js', {
-  workerData: {
-    path: './workers/tick.ts'
-  }
-});
+// const tick = new Worker('./worker.js', {
+//   workerData: {
+//     path: './workers/tick.ts'
+//   }
+// });
 
-let input: string = '';
+let searchbar: HTMLInputElement = (document.getElementById('searchbar') as HTMLInputElement);
+let input: string;
 let searchDelay: number = 1000;
 let ticksSinceLastChange: number = 0;
 
-function updateInput(elem: HTMLInputElement) {
-  input = elem.value;
+function inputChanged() {
+  input = searchbar.value;
   ticksSinceLastChange = 0;
 }
 
-function search() {
-  console.log(`Searching: "${ input }"`);
-}
+searchbar.addEventListener('oninput', inputChanged);
 
-tick.on('message', (result) => {
-  if (result == 'tick') {
-    if (ticksSinceLastChange != -1) {
-      ticksSinceLastChange = ticksSinceLastChange + 1;
-    }
+// function updateInput(elem: HTMLInputElement) {
+//   input = elem.value;
+//   ticksSinceLastChange = 0;
+// }
+
+// function search() {
+//   console.log(`Searching: "${ input }"`);
+// }
+
+// tick.on('message', (result) => {
+//   if (result == 'tick') {
+//     if (ticksSinceLastChange != -1) {
+//       ticksSinceLastChange = ticksSinceLastChange + 1;
+//     }
     
-    if (ticksSinceLastChange >= searchDelay) {
-      ticksSinceLastChange = -1;
-      search();
-    }
-  }
-});
+//     if (ticksSinceLastChange >= searchDelay) {
+//       ticksSinceLastChange = -1;
+//       search();
+//     }
+//   }
+// });
